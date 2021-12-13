3085b258ab36ecdcbd0816bc3234bbfe20a5482d
dåshuång li <li.dashuang@icloud.com>
2021-12-13 15:00:50 +0800

    Update log_level.ex
    https://exercism.org/tracks/elixir/exercises/log-level

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,9 +1,33 @@
 defmodule LogLevel do
-  def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+  def to_label(level, true) do
+    case level do
+      1 -> :debug
+      2 -> :info
+      3 -> :warning
+      4 -> :error
+      _ -> :unknown
+    end
+  end
+
+  def to_label(level, false) do
+    case level do
+      0 -> :trace
+      1 -> :debug
+      2 -> :info
+      3 -> :warning
+      4 -> :error
+      5 -> :fatal
+      _ -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    label = to_label(level, legacy?)
+    cond do
+      label == :error || label == :fatal -> :ops
+      label == :unknown && legacy? -> :dev1
+      label == :unknown && !legacy? -> :dev2
+      true -> false
+    end
   end
 end
