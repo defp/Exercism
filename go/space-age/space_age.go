de7af12374c85c9b83585ae5b861afa72f39c049
dåshuång li <li.dashuang@icloud.com>
2022-01-12 19:16:36 +0800

    https://exercism.org/tracks/go/exercises/space-age

---
+++ go/space-age/space_age.go
@@ -0,0 +1,23 @@
+package space
+
+type Planet string
+
+var planets = map[Planet]float64{
+	"Earth":   31557600,
+	"Mercury": 31557600 * 0.2408467,
+	"Venus":   31557600 * 0.61519726,
+	"Mars":    31557600 * 1.8808158,
+	"Jupiter": 31557600 * 11.862615,
+	"Saturn":  31557600 * 29.447498,
+	"Uranus":  31557600 * 84.016846,
+	"Neptune": 31557600 * 164.79132,
+}
+
+
+func Age(seconds float64, planet Planet) float64 {
+  	period, ok := planets[planet]
+        if (ok) {
+          return seconds / period
+        }
+        return 0
+}
